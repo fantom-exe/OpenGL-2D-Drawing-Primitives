d4e35e59667372b3565b50ce1761c1c5f4399f58
Ed <eisajanyan@gmail.com>
2019-03-26 11:49:51 -0700

    first commit

---
+++ Isajanyan_Program1.java
@@ -0,0 +1,192 @@
+/***************************************************************
+ * file: Isajanyan_Program1.java
+ * author: Edward Isajanyan
+ * class: CS 4450
+ *
+ * assignment: program 1
+ * date: 2/11/2019
+ *
+ * purpose: Reads coordinates from a text file 'coordinates.txt'
+ * and draws the primitives in a window.
+ *
+ ****************************************************************/
+
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import static org.lwjgl.opengl.GL11.*;
+
+
+public class Isajanyan_Program1 {
+	
+	// start
+	private void start() {
+		try {
+			// init primitives
+			readCoordinates();
+			
+			// begin render
+			createWindow();
+			initGL();
+			render();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// read coordinates from file
+	private void readCoordinates() throws FileNotFoundException {
+		Scanner scanner = new Scanner(new File("coordinates.txt"));
+		String   line;
+		String[] tokens;
+		
+		Polygon polygon = new Polygon();
+		
+		while(scanner.hasNextLine()) {
+			line   = scanner.nextLine();
+			tokens = line.split(" ");
+			
+			if(tokens[0].equals("P")) {
+				polygon = new Polygon(); // create new polygon
+				polygon.setColor(Float.parseFloat(tokens[1]),
+				                 Float.parseFloat(tokens[2]),
+				                 Float.parseFloat(tokens[3])); // store color
+				
+				do { // store vertices
+					line = scanner.nextLine(); // go to next line
+					tokens = line.split(" ");
+					
+					polygon.addVertex(Integer.parseInt(tokens[0]),
+					                  Integer.parseInt(tokens[1])); // store vertices
+				} while(scanner.hasNextInt());
+				
+				polygonArray.add(polygon); // add polygon to array
+			}
+			else if(tokens[0].equals("T")) {
+				do { // store transitions
+					line = scanner.nextLine(); // go to next line
+					tokens = line.split(" ");
+					
+					switch(tokens[0].charAt(0)) {
+						case 'r':
+							polygon.addRotation(Integer.parseInt(tokens[1]),
+							                    Integer.parseInt(tokens[2]),
+							                    Integer.parseInt(tokens[3]));
+							break;
+						case 's':
+							polygon.addScaling(Float.parseFloat(tokens[1]),
+							                   Float.parseFloat(tokens[2]),
+							                   Float.parseFloat(tokens[3]),
+							                   Float.parseFloat(tokens[4]));
+							break;
+						case 't':
+							polygon.addTranslation(Integer.parseInt(tokens[1]),
+							                       Integer.parseInt(tokens[2]));
+							break;
+					}
+				} while(!scanner.hasNext("[P]") && scanner.hasNextLine());
+				
+			}
+			
+		} // while
+		
+		scanner.close();
+	}
+	
+	// init GL
+	private void initGL() {
+		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+		
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		
+		glOrtho(-320, 320, -240, 240, 1, -1);
+		
+		glMatrixMode(GL_MODELVIEW);
+		glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+	}
+	
+	// render
+	private void render() {
+		// render loop
+		while(!Display.isCloseRequested() && !Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) {
+			try {
+				glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+				glLoadIdentity( );
+				
+				// draw primitives
+				
+				
+				Display.update();
+				Display.sync(60);
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		Display.destroy( );
+	}
+	
+	// draw given polygon obj
+	private void draw() {
+		glColor3f(polygon.getColorAt(0), polygon.getColorAt(1), polygon.getColorAt(2));
+		glPointSize(10);
+		
+		glBegin(GL_POLYGON);
+		polygon.vertices.forEach(ints -> glVertex2f(ints[0], ints[1]));
+		glEnd( );
+	}
+	
+	// create window
+	private void createWindow() throws Exception{
+		Display.setFullscreen(false);
+		
+		Display.setDisplayMode(new DisplayMode(640, 480));
+		Display.setTitle("Isajanyan_Program1");
+		Display.create( );
+	}
+	
+	// line
+	private void drawLine(int x1, int y1, int x2, int y2) {
+		glColor3f(1.0f, 0.0f, 0.0f);
+		glPointSize(10);
+		
+		glBegin(GL_LINE_LOOP);
+		glVertex2f(150, 200);
+		glVertex2f(80, 145);
+		glEnd( );
+	}
+	
+	// circle
+	private void drawCircle(int x, int y, int r) {
+		glColor3f(0.0f, 0.0f, 1.0f);
+		glPointSize(5);
+		
+		glBegin(GL_LINE_LOOP);
+		glVertex2f(150, 200);
+		glVertex2f(80, 145);
+		glEnd( );
+	}
+	
+	// ellipse
+	private void drawEllipse(int x, int y, int rx, int ry) {
+		glColor3f(0.0f, 1.0f, 0.0f);
+		glPointSize(5);
+		
+		glBegin(GL_LINE_LOOP);
+		glVertex2f(150, 200);
+		glVertex2f(80, 145);
+		glEnd( );
+	}
+	
+	// main
+	public static void main(String[] args) {
+		Isajanyan_Program1 program1 = new Isajanyan_Program1();
+		program1.start();
+	}
+}
