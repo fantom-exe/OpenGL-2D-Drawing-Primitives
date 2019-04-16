32bf4094a5962584e7fa683d7b1a8545d787569d
Ed <eisajanyan@gmail.com>
2019-04-15 21:21:17 -0700

    plotLineLow IP

--- Isajanyan_Program1.java
+++ Isajanyan_Program1.java
@@ -271,20 +271,24 @@ public class Isajanyan_Program1 {
               dy = y1 - y0,
               yi = 1;
         
-        //  if dy < 0
-        //    yi = -1
-        //    dy = -dy
-        //  end if
-        //  D = 2*dy - dx
-        //  y = y0
-        //
-        //  for x from x0 to x1
-        //    plot(x,y)
-        //    if D > 0
-        //       y = y + yi
-        //       D = D - 2*dx
-        //    end if
-        //    D = D + 2*dy
+        if (dy < 0) {
+            yi = -1;
+            dy = -dy;
+        }
+        
+        float d = 2*dy - dx,
+              y = y0;
+
+        for (x from x0 to x1){
+            plot(x, y);
+            
+            if (d > 0) {
+                y = y + yi;
+                d -= 2 * dx;
+            }
+            
+            d += 2 * dy;
+        }
     }
     
     private void plotLineHigh(float x1, float y1, float x0, float y0) {
